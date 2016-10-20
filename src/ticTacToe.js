32f0dfb5c6aeac525c4fc9ccb301470be0638041
Matthew Perkins <mfperkins@gmail.com>
2016-10-20 12:50:23 +0100

    added in empty arrays to store X and O moves

--- src/ticTacToe.js
+++ src/ticTacToe.js
@@ -1,12 +1,26 @@
 function Game() {
   this._winners = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]];
   this._remainingMoves = [1,2,3,4,5,6,7,8,9];
+  this._xMoves = [];
+  this._oMoves = [];
 }
 
-Game.prototype.getWinners = function() {
-  return this._winners;
-};
+Game.prototype = {
 
-Game.prototype.getRemainingMoves = function() {
-  return this._remainingMoves;
+  getWinners: function() {
+    return this._winners;
+  },
+
+  getRemainingMoves: function() {
+    return this._remainingMoves;
+  },
+
+  getXMoves: function() {
+    return this._xMoves;
+  },
+
+  getOMoves: function() {
+    return this._oMoves;
+  },
+
 };
