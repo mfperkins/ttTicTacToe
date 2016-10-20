eafc3fc852c69dfa8f1ea9e48482aad9a764915e
Matthew Perkins <mfperkins@gmail.com>
2016-10-20 19:35:45 +0100

    adding in game over check

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -2,6 +2,10 @@ describe("Game", function() {
 
   var game;
   var winners = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]];
+  var firstMoves = [1,4,7];
+  var secondMoves = [7,9,8];
+  var allMoves = [1,2,3,4,5,6,7,8,9];
+  moves = jasmine.createSpyObj("moves", ['play', 'getRemainingMoves']);
 
   beforeEach(function() {
     game = new Game();
@@ -14,5 +18,30 @@ describe("Game", function() {
     });
 
   });
+
+  describe("#move", function () {
+
+    it("should send the number to the Moves object ", function () {
+      game.move(firstMoves[0]);
+      expect(moves.play).toHaveBeenCalledWith(1);
+    });
+
+  });
+
+  describe("#move", function () {
+
+    beforeEach(function(){
+      for (var i = 0; i < allMoves.length-1; i++) {
+        game.move(firstMoves[i]);
+      }
+      moves.getRemainingMoves.and.returnValue([]);
+    });
+
+    it("should declare 'Game Over' if movesRemaining === 0", function () {
+      expect(game.move(9)).toEqual("Game Over");
+    });
+
+  });
+
 
 });
--- src/game.js
+++ src/game.js
@@ -1,11 +1,24 @@
 function Game() {
   this._winners = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]];
+  this._moves = (typeof moves !== 'undefined') ?  moves : new Moves();
 }
 
 Game.prototype = {
 
   getWinners: function() {
     return this._winners;
+  },
+
+  move: function (number) {
+    this._moves.play(number);
+    if (this._isGameOver() !== null ) { return this._isGameOver(); }
+
+
+
+  },
+
+  _isGameOver: function() {
+    if (this._moves.getRemainingMoves.length === 0) { return "Game Over" ;}
   },
 
 };
